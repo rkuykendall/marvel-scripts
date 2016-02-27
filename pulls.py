2f12acf2fa5ddcfda429d4beec661228862dd1df
Robert Kuykendall <robert@rkuykendall.com>
2016-02-27 14:59:01 -0500

    pulls.py is now fully running on marvelous!!!

--- pulls.py
+++ pulls.py
@@ -1,30 +1,28 @@
 import os
-from marvel.marvel import Marvel
+import marvelous
 from config import public_key, private_key
 
 IGNORE = set([
-    19709, 20256, 19379, 19062, 19486, 19242, 19371, 19210, 20930, 21328, 20834,
-    18826, 20933, 20365, 20928, 21129, 20786, 21402, 21018
+    19709, 20256, 19379, 19062, 19486, 19242, 19371, 19210, 20930, 21328,
+    20834, 18826, 20933, 20365, 20928, 21129, 20786, 21402, 21018
 ])
 
-m = Marvel(public_key, private_key)
+m = marvelous.api(public_key, private_key)
 
-pulls = sorted(m.get_comics(
-    format="comic",
-    formatType="comic",
-    noVariants=True,
-    dateDescriptor="thisWeek").data.results,
+pulls = sorted(m.comics({
+    'format': "comic",
+    'formatType': "comic",
+    'noVariants': True,
+    'dateDescriptor': "thisWeek"}),
     key=lambda comic: comic.title)
 
-on_sale = [d for d in pulls[0].dates if d.type == 'onsaleDate'][0].date
-directory = on_sale.strftime('%m-%d')
+directory = pulls[0].dates.on_sale.strftime('%m-%d')
 
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 with open(directory + '/pulls.txt', 'w') as pull_checklist:
     for comic in pulls:
-        series_num = int(comic.series['resourceURI'].split('/')[-1])
-        if series_num not in IGNORE:
+        if comic.series.id not in IGNORE:
             pull_checklist.write('{} (series #{})\n'.format(
-                comic.title.encode('utf-8'), series_num))
+                comic.title.encode('utf-8'), comic.series.id))
