015f1adcf8b0b19fe42eb512205e813448c4b1ba
Robert Kuykendall <robert@rkuykendall.com>
2016-02-28 02:28:14 -0500

    Convert final script, reading_order.py, to marvelous!

--- reading_order.py
+++ reading_order.py
@@ -1,10 +1,10 @@
 import sys
 import os
 from collections import defaultdict
-from marvel.marvel import Marvel
+import marvelous
 from config import public_key, private_key
 
-m = Marvel(public_key, private_key)
+m = marvelous.api(public_key, private_key, cache=True)
 
 series = sys.argv[1:]
 
@@ -15,23 +15,23 @@ def ordinal(n):
        return  str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, "th")
 
 def all_comics_for_series(id):
-    series = m.get_single_series(id).data.results[0]
+    series = m.series(id)
 
     LIMIT = 100
     offset = 0
     total = None
     comics = []
     while total is None or len(comics) < total:
-        response = series.get_comics(
-            format='comic',
-            formatType='comic',
-            noVariants=True,
-            limit=LIMIT,
-            offset=offset,
-            orderBy='issueNumber'
-        ).data
-        comics += response.results
-        total = response.total
+        response = series.comics({
+            'format': 'comic',
+            'formatType': 'comic',
+            'noVariants': True,
+            'limit': LIMIT,
+            'offset': offset,
+            'orderBy': 'issueNumber'
+        })
+        comics += response.comics
+        total = response.response['data']['total']
         offset += LIMIT
 
     return comics
@@ -40,9 +40,8 @@ comics_ordered = defaultdict(list)
 
 for series_id in series:
     for comic in all_comics_for_series(series_id):
-        comic_date = [d for d in comic.dates if d.type == 'onsaleDate'][0].date
-        comic_title = comic.title
-        comics_ordered[comic_date].append(comic_title)
+        if comic.dates.on_sale is not None:
+            comics_ordered[comic.dates.on_sale].append(comic.title)
         
 for date in sorted(comics_ordered):
     for comic in comics_ordered[date]:
